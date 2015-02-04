9f5c46496d8c1471b75c9a0af08b2aabdfef61fe
Aaron Bradley <abradle2@gmail.com>
2015-02-04 12:42:04 -0800

    added roi error to prediction algorithms

--- load_data_from_db.py
+++ load_data_from_db.py
@@ -13,7 +13,7 @@ with open('credentials.json') as credentials_file:
     credentials = json.load(credentials_file)
 
 passwd = credentials['mysql']['password']
-con = mdb.connect(host='127.0.0.1', port=3307, user='root', passwd=passwd, db='insight', autocommit=True)
+con = mdb.connect(host='127.0.0.1', port=3306, user='root', passwd=passwd, db='insight', autocommit=True)
 
 features_defaults = [
 			"id",
